ffbf757e21f49c9105a82b4c97f9f0b2a28d0da2
Medienpaed-Games <anna-katharina@findel.de>
2024-04-27 18:11:17 +0200

    [ADDED]= erste Versuche für Inventarsystem, Mausklickposition

---
+++ harz_mythen/Assets/Scenes/09_Scripts/MouseClick.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseClick : MonoBehaviour
+{
+    
+
+    public InventoryObject inventory;
+
+    
+
+    /*
+    public void OnMouseUpAsButton()
+    {
+        var item = other.GetComponent<Item>();
+        if (item)
+        {
+            inventory.AddItem(item.item, 1);
+            Destroy(other.gameObeject);
+        }
+    }
+    */
+}
