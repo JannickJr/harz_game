fa7c08570eb49a7009765434596dbc0a195c17eb
Medienpaed-Games <anna-katharina@findel.de>
2024-04-30 16:39:36 +0200

    [CHANGED] alle Assets tauchen wieder im Inventar auf

--- harz_mythen/Assets/Scenes/09_Scripts/Inventory_woSO/ItemSlot.cs
+++ harz_mythen/Assets/Scenes/09_Scripts/Inventory_woSO/ItemSlot.cs
@@ -67,7 +67,6 @@ public class ItemSlot : MonoBehaviour, IPointerClickHandler
             selectedShader.SetActive(false);
             thisItemSelected = false;
         }
-        
     }
     public void OnRightClick()
     {
