b81a408b087dcbe3eef11fac50260e269b3fbb6e
Medienpaed-Games <anna-katharina@findel.de>
2024-07-01 20:07:32 +0200

    [ADDED] Kamerabewegungsstopp am Rand funktioniert jetzt richtig

--- harz_mythen/Assets/09_Scripts/Camera_Movement/CameraMovement_Scene_1.cs
+++ harz_mythen/Assets/09_Scripts/Camera_Movement/CameraMovement_Scene_1.cs
@@ -9,32 +9,42 @@ public class CameraMovement_Scene_1 : MonoBehaviour
     [SerializeField] private Slider slider;
     [SerializeField] private float movementSpeedCamera;
 
-
+    [SerializeField] private Transform leftWall;
+    [SerializeField] private Transform rightWall;
+  
     private void Update()
     {
-        float movement = slider.value;
-        mainCamera.transform.position += new Vector3(movement, 0, 0) * movementSpeedCamera * Time.deltaTime;
-        //StopMovement();
+        if (slider.value != 0)
+        {
+            MoveCamera();
+        }
     }
-    // Wenn Slider negativ + zu nah am linken Empty, dann nicht weiter in diese Richtung bewegen;
-    // Wenn Slider positiv + zu nah am rechten Empty, dann nicht weiter in diese Richtung bewegen; 
+    // Wenn Slider negativ + Kamera zu nah am linken Empty, dann Slider nicht weiter in diese Richtung bewegen;
+    // Wenn Slider positiv + Kamera zu nah am rechten Empty, dann Slider nicht weiter in diese Richtung bewegen; 
     // eine Koordinate angucken: Ist x-Koordinate größer oder kleiner als Ecken (= Sitz der Emptys), die ich gesetzt habe?
+    // wenn nicht gedrückt, dann nicht bewegen
+
+    public void MoveCamera()
+    {
+        float movement = slider.value;
+
+        mainCamera.transform.position += new Vector3(movement, 0, 0) * movementSpeedCamera * Time.deltaTime;
+
+        if (slider.value <= 0 && mainCamera.transform.position.x <= leftWall.position.x)
+        {
+            Debug.Log("Es ist soweit."); // wird erkannt
+            mainCamera.transform.position = leftWall.position;
+
+        }
+        else if (slider.value >= 0 && mainCamera.transform.position.x >= rightWall.position.x)
+        {
+            mainCamera.transform.position = rightWall.position;
+        }
+    }
+
     public void resetSlider()
     {
         slider.value = 0;
-    }
-
-    public void StopMovement()
-    {
-        mainCamera.transform.position = new Vector3(1.37f, 0, 0);
-        
-        //float movement = 0;
-    }
-
-    public void OnCollisionEnter(Collision other)
-    {
-        Debug.Log("Treffer");
-        //movementSpeedCamera = 0;
     }
 }
 
