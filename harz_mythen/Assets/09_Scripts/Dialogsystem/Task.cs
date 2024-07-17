aba1321fc47ac0002108df5e8b56593ae8269614
Medienpaed-Games <anna-katharina@findel.de>
2024-07-18 01:20:15 +0200

    [ADDED] Aufgabe blendet aus

--- harz_mythen/Assets/09_Scripts/Dialogsystem/Task.cs
+++ harz_mythen/Assets/09_Scripts/Dialogsystem/Task.cs
@@ -1,21 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Task : MonoBehaviour
 {
+    [SerializeField] private Image box;
+    [SerializeField] private TextMeshProUGUI text;
+    [SerializeField] private GameObject task_1;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        //DestroyTask();
+        StartCoroutine(FadeOut());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator FadeOut()
     {
-        
+        yield return new WaitForSeconds(4f);
+        box.CrossFadeAlpha(0, 5, false);
+        text.CrossFadeAlpha(0, 5, false);
+        yield return new WaitForSeconds(5f);
+        StopAllCoroutines();
+        task_1.SetActive(false);
     }
-
-
-
 }
