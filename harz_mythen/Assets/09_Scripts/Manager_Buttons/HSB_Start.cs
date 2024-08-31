cf618b8c1e80a61deddcdfa1843d3ec7c68bdb3b
Medienpaed-Games <anna-katharina@findel.de>
2024-08-31 22:57:10 +0200

    [CHANGED] Ort des Sounds

--- harz_mythen/Assets/09_Scripts/Manager_Buttons/HSB_Start.cs
+++ harz_mythen/Assets/09_Scripts/Manager_Buttons/HSB_Start.cs
@@ -6,10 +6,12 @@ public class HSB_Start : MonoBehaviour
 {
     private Animator animator;
     //[SerializeField] GameObject weiter;
+    private AudioSource Audio;
 
     void Start()
     {
         animator = GetComponent<Animator>();
         animator.Play("HSB");
+        Audio = GetComponent<AudioSource>();
     }
 }
