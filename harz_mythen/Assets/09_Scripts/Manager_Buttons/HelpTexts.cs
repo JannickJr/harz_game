9d4e5a0e8b07242d55d74d4ae86ea437d4e766bf
Medienpaed-Games <anna-katharina@findel.de>
2024-08-31 22:38:07 +0200

    [CHANGED] Walls, Hilfetexte

--- harz_mythen/Assets/09_Scripts/Manager_Buttons/HelpTexts.cs
+++ harz_mythen/Assets/09_Scripts/Manager_Buttons/HelpTexts.cs
@@ -31,19 +31,19 @@ public class HelpTexts : MonoBehaviour
         if (GameObject.Find("03_Scene_01"))
         {
             help_1.text = "Die Truhe ist tief im Wald versteckt.";
-            help_2.text = "Wie oft findest du die Farben auf den Zahn-\nrädern auf den Blumen wieder?";
-            solution.text = "weiß: 1 \nlila: 5 \ngelb: 4 \nrot: 0";
+            help_2.text = "Die Farben auf den Zahnrädern stehen in Zusammenhang mit den Blumen im Wald."; // Wie oft findest du die Farben auf den Zahn-\nrädern auf den Blumen wieder?
+            solution.text = "weiß: 2 \nlila: 2 \ngelb: 2 \nblau: 4";
         } 
         if (GameObject.Find("05_Scene_02"))
         {
             help_1.text = "Die Kristalle bergen eine magische Kraft.";
-            help_2.text = "Die Kristalle müssen in der Reihenfolge der Melodie angeklickt werden.";
-            solution.text = "...";
+            help_2.text = "Sie müssen in der richtigen Reihenfolge angeklickt werden.";
+            solution.text = "Die richtige Reihenfolge steht auf dem Wagen.";
         }
         if (GameObject.Find("07_Scene_03"))
         {
             help_1.text = "Die Sterne leuchten ganz hell.";
-            help_2.text = "noch unbekannt";
+            help_2.text = "Die Sterne müssen in die richtige Form geschoben werden.";
             solution.text = "...";
         }
     }
